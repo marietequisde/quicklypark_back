5a93e674c8218a94eb2bc1a2239670aecd35efea
torrijomario@gmail.com <torrijomario@gmail.com>
2026-05-22 17:13:29 +0200

    Las plazas con planta no se devuelven.

--- src/main/java/com/quicklypark/back/acceso/repository/PlazaRepository.java
+++ src/main/java/com/quicklypark/back/acceso/repository/PlazaRepository.java
@@ -10,6 +10,6 @@ import com.quicklypark.back.acceso.entity.PlazaEntity;
 
 public interface PlazaRepository extends CrudRepository<PlazaEntity, Long> {
 
-	@Query("FROM PlazaEntity p WHERE p.idParking = :idParking")
+	@Query("FROM PlazaEntity p WHERE p.idParking = :idParking AND planta = 0")
 	public List<PlazaEntity> findByIdParking(@Param("idParking") long idParking);
 }
